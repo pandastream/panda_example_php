5f22dc09c8ded1cc59941b71df9838ece18a3879
Pablo Brasero and Vivien Schilis <bambinos+pablobm+vivienschilis@new-bamboo.co.uk>
2010-02-02 11:51:55 +0000

    Styled filename field

--- index.php
+++ index.php
@@ -6,10 +6,10 @@ include('lib/head.inc.html');
 <p>This is an example <strong>Panda</strong> client application, written in <strong>PHP</strong>.</p>
 <form action="/player.php" method="get" id="upload-form">
     <label>Upload a video<br/></label>
-
+    
     <span id="upload_button"></span>
-    <input type="text" id="upload_filename" disabled="true" />
-    <div id="upload_progress" class="progress_bar_container"></div>
+    <input type="text" id="upload_filename" disabled="true" class="panda_upload_filename" />
+    <div id="upload_progress" class="panda_upload_progress"></div>
     <input type="hidden" id="returned_video_id" name="panda_video_id" />
 
 <script type="text/javascript">
@@ -19,8 +19,6 @@ $('#returned_video_id').pandaUploader(<?php echo json_encode(@$panda->signed_par
     upload_progress_id: 'upload_progress'
 });
 </script>
-        
-		<p><input type="submit" value="Save" /></p>
-		
+    <p><input type="submit" value="Save" /></p>	
 </form>
 <?php include('lib/foot.inc.html'); ?>
