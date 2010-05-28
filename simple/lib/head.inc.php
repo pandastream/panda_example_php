76de0bc7695ea6f2d0f566a04e2c32d4815b2e83
Pablo Brasero <pablo@new-bamboo.co.uk>
2010-05-28 12:40:16 +0100

    Updated to panda_uploader 0.8

--- simple/lib/head.inc.php
+++ simple/lib/head.inc.php
@@ -4,7 +4,7 @@
 	<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
 	<title>Panda: example of PHP-based client app</title>
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js"></script>
-	<script src="<?php echo BASE_URL ?>/panda_uploader/jquery.panda-uploader-0.4.min.js"></script>
+	<script src="<?php echo BASE_URL ?>/panda_uploader/jquery.panda-uploader-0.8.min.js"></script>
 	<link href="<?php echo BASE_URL ?>/panda_uploader/panda-uploader.css" rel="stylesheet" type="text/css">
 </head>
 <body>
