853cdee4cfa8b96d4ea9aeb9176cb7d44bac2ba2
Aaron McNeal <aaron@mediacore.com>
2012-04-09 13:52:11 -0700

    Update Javascript imports to use CDN

--- simple/lib/head.inc.php
+++ simple/lib/head.inc.php
@@ -4,6 +4,6 @@
 	<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
 	<title>Panda: example of PHP-based client app</title>
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js"></script>
-	<script src="<?php echo BASE_URL ?>/panda_uploader/jquery.panda-uploader-1.0.1.min.js"></script>
+	<script src="//d21qbsp9m249bi.cloudfront.net/u/1.4/jquery.panda-uploader.min.js" type="text/javascript"></script>
 </head>
 <body>
